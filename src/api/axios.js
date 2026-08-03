eae1117f21eafb300c13e99bb3d199dc5be6e3f1
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-08-03 19:30:55 +0300

    axios.js is now finish with both interceptors working good

--- src/api/axios.js
+++ src/api/axios.js
@@ -25,11 +25,37 @@ api.interceptors.response.use(
     },
     async (error) => {
         const originalRequest = error.config
+        const refresh = localStorage.getItem("refresh")
 
-        return Promise.reject(error)
+        if (error.response?.status === 401) {
+            console.log("e 401, trebuie refresh")
+
+            if (!refresh) {
+            console.log("You need to re-authenticate")
+            return Promise.reject(error)
+        }
+
+            
+            try {
+                
+                const response = await axios.post("http://127.0.0.1:8000/api/token/refresh/", {refresh})
+
+                localStorage.setItem("access", response.data.access)
+
+                originalRequest.headers.Authorization = `Bearer ${response.data.access}`
+                return api(originalRequest)
+            
+            } catch {
+                localStorage.removeItem("access")
+                localStorage.removeItem("refresh")
+                window.location.href = "/login"
+
+            } 
+        }
+return Promise.reject(error)
+
 }
+
 )
 
 export default api
-
-localStorage.getItem("refresh")
