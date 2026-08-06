2d11110db56a543f70694df94d72e6444171561a
Aurelian Buzincu <aurelianbuzincu5@gmail.com>
2026-08-06 17:55:37 +0300

    I did progres in ServicesPage.jsx but i have to complete the return. also added Services page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/barbers" element={<BarbersPage />} />
+        <Route path="/services/:barberId" element={<ServicesPage />} />
       </Routes>
     </>
   )
--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,4 +1,42 @@
+import api from "../api/axios"
+import { useState, useEffect } from "react"
+import FormError from "../components/FormError"
+import { useParams } from "react-router"
+
+
 function ServicesPage() {
+    const [errors, setErrors] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [services, setServices] = useState([])
+    const { barberId } = useParams()
+
+    async function fetchServices() {
+
+        setErrors({})
+        setLoading(true)
+
+        try{
+            const response = await api.get(`services/?barber=${barberId}`)
+            setServices(response.data)
+        } catch (error){
+            if (error.response){
+                setErrors(error.response.data)
+            } else {
+                setErrors({general: "Could not connect to the server."})
+            }
+        } finally {
+            setLoading(false)
+        }
+        }
+
+    useEffect(() => {
+        fetchServices()
+    },[])
+    
+
+
+
+
     return(
         <h1>ServicesPage</h1>
     )
